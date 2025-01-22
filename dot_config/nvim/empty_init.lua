2c4b541b9b6717fc1da25546ace50eb889da7431
Franemar <franklin.e.marin@gmail.com>
2025-01-22 15:49:13 -0300

    Nvim tree-sitter plugin attempt.

---
+++ dot_config/nvim/empty_init.lua
@@ -0,0 +1,13 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Plugins
+Plug ('nvim-treesitter/nvim-treesitter'), {['do'] = ':TSUpdate'})
+
+vim.call('plug#end')
+
+-- Color schemes should be loaded after plug#end().
+-- We prepend it with 'silent!' to ignore errors when it's not yet installed.
+-- vim.cmd('silent! colorscheme seoul256')
