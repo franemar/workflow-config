b5ff2aaaafd67a88a5090aaefe1829468c40ad32
Franemar <franklin.e.marin@gmail.com>
2025-03-13 21:37:52 -0300

    lazy.lua: dropped unused tree-sitter languages and tasklite config.yaml added.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
       config = function ()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { "lua", "vim", "vimdoc", "query", "heex", "javascript", "html", "kotlin", "clojure" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
